83e003ce0d0e740c255ea190116450320a338347
Dennis Heine <dennis.heine@gmx.de>
2020-06-08 18:42:51 +0200

    Uploaded sourcecode

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbKey = new System.Windows.Forms.ComboBox();
+            this.bnPixel = new System.Windows.Forms.Button();
+            this.bnAdd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.bnDelete = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.bnStart = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbKey);
+            this.groupBox1.Controls.Add(this.bnPixel);
+            this.groupBox1.Controls.Add(this.bnAdd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDescription);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 313);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 111);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Macro";            
+            // 
+            // cbKey
+            // 
+            this.cbKey.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbKey.FormattingEnabled = true;
+            this.cbKey.Location = new System.Drawing.Point(78, 49);
+            this.cbKey.Name = "cbKey";
+            this.cbKey.Size = new System.Drawing.Size(100, 21);
+            this.cbKey.TabIndex = 7;
+            // 
+            // bnPixel
+            // 
+            this.bnPixel.Location = new System.Drawing.Point(6, 78);
+            this.bnPixel.Name = "bnPixel";
+            this.bnPixel.Size = new System.Drawing.Size(75, 23);
+            this.bnPixel.TabIndex = 6;
+            this.bnPixel.Text = "Set Pixel";
+            this.bnPixel.UseVisualStyleBackColor = true;
+            this.bnPixel.Click += new System.EventHandler(this.bnPixel_Click);
+            // 
+            // bnAdd
+            // 
+            this.bnAdd.Enabled = false;
+            this.bnAdd.Location = new System.Drawing.Point(103, 78);
+            this.bnAdd.Name = "bnAdd";
+            this.bnAdd.Size = new System.Drawing.Size(75, 23);
+            this.bnAdd.TabIndex = 5;
+            this.bnAdd.Text = "Add";
+            this.bnAdd.UseVisualStyleBackColor = true;
+            this.bnAdd.Click += new System.EventHandler(this.bnAdd_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Key to press";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(78, 27);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(100, 20);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.bnDelete);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(190, 285);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Macros";
+            // 
+            // bnDelete
+            // 
+            this.bnDelete.Location = new System.Drawing.Point(0, 256);
+            this.bnDelete.Name = "bnDelete";
+            this.bnDelete.Size = new System.Drawing.Size(184, 23);
+            this.bnDelete.TabIndex = 4;
+            this.bnDelete.Text = "Delete";
+            this.bnDelete.UseVisualStyleBackColor = true;
+            this.bnDelete.Click += new System.EventHandler(this.bnDelete_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 238);
+            this.listBox1.TabIndex = 2;
+            // 
+            // bnStart
+            // 
+            this.bnStart.Location = new System.Drawing.Point(6, 430);
+            this.bnStart.Name = "bnStart";
+            this.bnStart.Size = new System.Drawing.Size(184, 23);
+            this.bnStart.TabIndex = 2;
+            this.bnStart.Text = "Start";
+            this.bnStart.UseVisualStyleBackColor = true;
+            this.bnStart.Click += new System.EventHandler(this.bnStart_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripSeparator1,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(196, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::WindowsFormsApp1.Properties.Resources.newpng;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::WindowsFormsApp1.Properties.Resources.open;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::WindowsFormsApp1.Properties.Resources.save;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = global::WindowsFormsApp1.Properties.Resources.help;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Clickz0r Profile (*.dat)|*.dat|DAT (*.dat)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Clickz0r Profile (*.dat)|*.dat|DAT (*.dat)|*.*";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 462);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.bnStart);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bnAdd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button bnDelete;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button bnPixel;
+        private System.Windows.Forms.Button bnStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox cbKey;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,303 @@
+﻿//using FMUtils.KeyboardHook;
+using FMUtils.KeyboardHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            this.Text = RandomString(8);
+            InitializeComponent();
+
+        }
+
+        #region Declarations
+        Hook KeyboardHook = null;
+        private bool isRunning;
+
+        public Dictionary<String, itemData> items = new Dictionary<string, itemData>();
+        Point pixelPoint = new Point();
+        Color pixelColor = new Color();
+        public static Dictionary<String, String> values = new Dictionary<string, string>();
+        public static Dictionary<String, Dictionary<String, String>> macros = new Dictionary<String, Dictionary<String, String>>();
+        bool doHook = false;
+
+        private static Random random = new Random();
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        public class itemData
+        {
+            public String text = "";
+            public WindowsInput.Native.VirtualKeyCode key = WindowsInput.Native.VirtualKeyCode.VK_0;
+            public override string ToString()
+            {
+                return text;
+            }
+        }
+        #endregion
+
+        #region Events
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            items.Add("0", new itemData() { text = "0", key = WindowsInput.Native.VirtualKeyCode.VK_0 });
+            cbKey.Items.Add(items["0"]);
+            items.Add("1", new itemData() { text = "1", key = WindowsInput.Native.VirtualKeyCode.VK_1 });
+            cbKey.Items.Add(items["1"]);
+            items.Add("2", new itemData() { text = "2", key = WindowsInput.Native.VirtualKeyCode.VK_2 });
+            cbKey.Items.Add(items["2"]);
+            items.Add("3", new itemData() { text = "3", key = WindowsInput.Native.VirtualKeyCode.VK_3 });
+            cbKey.Items.Add(items["3"]);
+            items.Add("4", new itemData() { text = "4", key = WindowsInput.Native.VirtualKeyCode.VK_4 });
+            cbKey.Items.Add(items["4"]);
+            items.Add("5", new itemData() { text = "5", key = WindowsInput.Native.VirtualKeyCode.VK_5 });
+            cbKey.Items.Add(items["5"]);
+            items.Add("6", new itemData() { text = "6", key = WindowsInput.Native.VirtualKeyCode.VK_6 });
+            cbKey.Items.Add(items["6"]);
+            items.Add("7", new itemData() { text = "7", key = WindowsInput.Native.VirtualKeyCode.VK_7 });
+            cbKey.Items.Add(items["7"]);
+            items.Add("8", new itemData() { text = "8", key = WindowsInput.Native.VirtualKeyCode.VK_8 });
+            cbKey.Items.Add(items["8"]);
+            items.Add("9", new itemData() { text = "9", key = WindowsInput.Native.VirtualKeyCode.VK_9 });
+            cbKey.Items.Add(items["9"]);
+
+            KeyboardHook = new Hook("Global Action Hook");
+            KeyboardHook.KeyUpEvent += KeyDown;
+        }
+
+
+        private void bnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtDescription.Text != "" && !macros.ContainsKey(txtDescription.Text))
+            {
+                int idx = listBox1.Items.Add(txtDescription.Text);
+                Dictionary<String, String> values = new Dictionary<string, string>();
+                values.Add("macro", cbKey.SelectedItem.ToString());
+
+                values.Add("pixelcolorR", pixelColor.R.ToString());
+                values.Add("pixelcolorG", pixelColor.G.ToString());
+                values.Add("pixelcolorB", pixelColor.B.ToString());
+                values.Add("pixelX", pixelPoint.X.ToString());
+                values.Add("pixelY", pixelPoint.Y.ToString());
+
+                macros.Add(txtDescription.Text, values);
+
+                bnAdd.Enabled = false;
+                txtDescription.Text = "";
+                cbKey.SelectedIndex = -1;
+            }
+            else
+            {
+                MessageBox.Show(this, "Please fill all fields.", "Error");
+            }
+        }
+
+
+        private void bnStart_Click(object sender, EventArgs e)
+        {
+            if (bnStart.Text == "Start")
+            {
+                isRunning = true;
+                backgroundWorker1.RunWorkerAsync();
+                bnStart.Text = "Stop";
+            }
+            else
+            {
+                bnStart.Text = "Start";
+                isRunning = false;
+            }
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (isRunning)
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    Dictionary<String, String> d = macros[listBox1.Items[i].ToString()];
+                    Point p = new Point(int.Parse(d["pixelX"]), int.Parse(d["pixelY"]));
+                    int R = int.Parse(d["pixelcolorR"]);
+                    int G = int.Parse(d["pixelcolorG"]);
+                    int B = int.Parse(d["pixelcolorB"]);
+
+                    String macro = d["macro"];
+                    if (checkPixel(p, R, G, B))
+                    {
+                        System.Threading.Thread.Sleep(RandomNumber(300, 700));
+                        sendString(macro);
+                        break;
+                    }
+                }
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private void bnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                macros.Remove(listBox1.SelectedItem.ToString());
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+        
+
+        private void bnPixel_Click(object sender, EventArgs e)
+        {
+            doHook = true;
+            bnPixel.Enabled = false;
+        }
+        private void KeyDown(KeyboardHookEventArgs e)
+        {
+            if (e.Key == Keys.F11 && doHook)
+            {
+                POINT lpPoint;
+                GetCursorPos(out lpPoint);
+
+                pixelPoint = new Point(lpPoint.X, lpPoint.Y);
+                System.Threading.Thread.Sleep(3000);
+                do
+                {
+                    pixelColor = GetColorAt(lpPoint.X, lpPoint.Y);
+                } while ((int)pixelColor.R == 255 && (int)pixelColor.B == 255 && (int)pixelColor.G == 255);
+                bnPixel.Enabled = true;
+                doHook = false;
+                bnAdd.Enabled = true;
+            }
+
+            if (e.Key == Keys.F)
+            {
+                if (bnStart.Text == "Start")
+                {
+                    isRunning = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    bnStart.Text = "Stop";
+                }
+                else
+                {
+                    bnStart.Text = "Start";
+                    isRunning = false;
+                }
+            }
+        }
+        #endregion
+
+        #region Helper Functions
+        public static string RandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+        private void sendString(String text)
+        {
+            try
+            {
+                WindowsInput.Native.VirtualKeyCode c = items[text].key;
+                InputSimulator i = new InputSimulator();
+                i.Keyboard.KeyPress(c);
+                //SendKeys.Send(text);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        public static Color GetColorAt(int x, int y)
+        {
+            Bitmap b = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(b);
+            g.CopyFromScreen(0, 0, 0, 0, b.Size);
+            Color c = b.GetPixel(x, y);
+            return c;
+        }
+        private bool checkPixel(Point p, int R, int G, int B)
+        {
+            if (GetColorAt(p.X, p.Y).R == R && GetColorAt(p.X, p.Y).G == G && GetColorAt(p.X, p.Y).B == B)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion
+
+        #region Menu Icons
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            macros = new Dictionary<string, Dictionary<string, string>>();
+            listBox1.Items.Clear();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                macros = SaveLoad.ReadFromBinaryFile<Dictionary<String, Dictionary<String, String>>>(openFileDialog1.FileName);
+                foreach (String d in macros.Keys)
+                {
+                    listBox1.Items.Add(d);
+                }
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                SaveLoad.WriteToBinaryFile<Dictionary<String, Dictionary<String, String>>>(saveFileDialog1.FileName, macros);
+            }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Usage:\r\n\r\nPress F11 to set the pixel after pressing 'Set Pixel'.\r\nTo Start/Stop, press the 'F' key.\r\n\r\nAttention! Change the filename of the executable to prevent detection.", "Help");
+        }
+        #endregion
+    }
+}
+;
